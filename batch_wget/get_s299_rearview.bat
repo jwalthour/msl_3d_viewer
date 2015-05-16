008aee14590f49c55d4ac2ef65d77196f2544b90
John Walthour <spaminous@yahoo.com>
2015-05-16 19:47:05 -0400

    Initial add

---
+++ batch_wget/get_s299_rearview.bat
@@ -0,0 +1,34 @@
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424045622EDR_F0060308RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424044407EDR_F0060302RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424043195EDR_F0060296RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424041970EDR_F0060290RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424039536EDR_F0060278RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424038322EDR_F0060272RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424035884EDR_F0060260RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424034670EDR_F0060254RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424033457EDR_F0060248RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RLB_424024283EDR_F0060224RHAZ00323M_.JPG
+
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424045622EDR_F0060308RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424044407EDR_F0060302RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424043195EDR_F0060296RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424041970EDR_F0060290RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424039536EDR_F0060278RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424038322EDR_F0060272RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424035884EDR_F0060260RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424034670EDR_F0060254RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424033457EDR_F0060248RHAZ00305M_.JPG
+wget http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00299/opgs/edr/rcam/RRB_424024283EDR_F0060224RHAZ00323M_.JPG
+
+
+
+
+
+
+
+
+
+
+
+
+
